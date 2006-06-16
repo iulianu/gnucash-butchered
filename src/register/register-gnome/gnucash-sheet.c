iulianu/gnucash-butchered

commit 310fad7522bb9829b5fc8240aac7834ff1f30913

Scott Oonk's patch to simplify the keyboard selection in gnucash-sheet.  Fixes a problem where selection does not work correctly when holding 'shift' and hitting the left arrow key.
git-svn-id: http://svn.gnucash.org/repo/gnucash/trunk@11892 57a11ea4-9604-0410-9ed3-97b8803252fd

src/register/register-gnome/gnucash-sheet.c
@@ -1567,7 +1567,6 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
 	CellBlock *header;
 	gboolean pass_on = FALSE;
         gboolean abort_move;
-        gboolean set_selection = TRUE;
         VirtualLocation cur_virt_loc;
         VirtualLocation new_virt_loc;
 	gncTableTraversalDir direction = 0;
@@ -1667,7 +1666,6 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
 		case GDK_Alt_L:
 		case GDK_Alt_R:
                         pass_on = TRUE;
-                        set_selection = FALSE;
                         break;
 		default:
                         if (gnucash_sheet_clipboard_event(sheet, event))
@@ -1688,64 +1686,12 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
                                         break;
                         }
 
-                        if (event->length > 0)
-                                set_selection = FALSE;
-
 			break;
         }
 
 	/* Forward the keystroke to the input line */
 	if (pass_on)
-        {
-                GtkEditable *editable;
-                gboolean extend_selection;
-                gboolean result;
-                gint current_pos;
-                gint start_sel;
-                gint end_sel;
-                gint new_pos;
-
-                editable = GTK_EDITABLE(sheet->entry);
-
-                current_pos = gtk_editable_get_position (editable);
-
-                extend_selection = event->state & GDK_SHIFT_MASK;
-                if (extend_selection && set_selection)
-                {
-			gtk_editable_get_selection_bounds (editable, &start_sel, &end_sel);
-                }
-                else
-                {
-                        start_sel = 0;
-                        end_sel = 0;
-                }
-
-                sheet->input_cancelled = FALSE;
-
-		result = gtk_widget_event (sheet->entry, (GdkEvent *) event);
-
-                sheet->input_cancelled = FALSE;
-
-                new_pos = gtk_editable_get_position (editable);
-
-                if (extend_selection && set_selection)
-                {
-                        if (start_sel == end_sel)
-                        {
-                                start_sel = current_pos;
-                                end_sel = new_pos;
-                        }
-                        else if (current_pos == start_sel)
-                                start_sel = new_pos;
-                        else
-                                end_sel = new_pos;
-                }
-
-                if (set_selection && (start_sel != end_sel))
-                        gtk_editable_select_region(editable, start_sel, end_sel);
-
-                return result;
-        }
+		return gtk_widget_event (sheet->entry, (GdkEvent *) event);
 
 	abort_move = gnc_table_traverse_update (table, cur_virt_loc,
                                                 direction, &new_virt_loc);

commit 22ecde6acc92679d6466f1dda9bf16bb6e20a76b

Restore non-editing state when cancelled editing is complete.    Fixes Bug #344870, which was a regression caused by r11892.
git-svn-id: http://svn.gnucash.org/repo/gnucash/trunk@14373 57a11ea4-9604-0410-9ed3-97b8803252fd

src/register/register-gnome/gnucash-sheet.c
@@ -195,6 +195,7 @@ gnucash_sheet_stop_editing (GnucashSheet *sheet)
         gnucash_sheet_hide_editing_cursor (sheet);
 
         sheet->editing = FALSE;
+        sheet->input_cancelled = FALSE;
 }
 
 